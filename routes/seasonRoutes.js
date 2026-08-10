011d1d9006b6e2cfd850baa1a007d76bc21033a6
RazvanEHB <razvan.tanase@student.ehb.be>
2026-08-10 14:33:38 +0200

    feat: implement delete route for seasons in seasonRoutes.js

--- routes/seasonRoutes.js
+++ routes/seasonRoutes.js
@@ -24,4 +24,24 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const season = await ImportSeason.findOneAndDelete({ id: Number(req.params.id) });
+        if (!season) {
+            return res.status(404).json({
+                error: 'Not Found',
+                message: `Season with ID ${req.params.id} not found, and could not be deleted.`
+            });
+        }
+        res.json({
+            message: 'Season deleted successfully.',
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: 'Internal Server Error',
+            message: 'Request could not be processed due to an internal server error.'
+        });
+    }
+});
+
 module.exports = router;
