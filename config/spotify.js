ba90848552fdb97a7edd36bfd40503fc0eb883ad
RazvanEHB <razvan.tanase@student.ehb.be>
2026-08-11 13:31:44 +0200

    feat: enhance getAccessToken function to include token retrieval logic from Spotify API, fix certain bugs and restructure

--- config/spotify.js
+++ config/spotify.js
@@ -10,6 +10,24 @@ async function getAccessToken() {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
     const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
     
+    const encoded = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
+
+    const body = new URLSearchParams({ grant_type: 'client_credentials' });
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Basic ${encoded}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: body
+    });
+
+    const data = await response.json();
+    accessToken = data.access_token;
+    tokenExpirationTime = Date.now() + data.expires_in * 1000;
+
+    return accessToken;
 }
 
 module.exports = getAccessToken;
